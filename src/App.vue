d66ab65e3216ba9b501151500ed6f95c79306733
Kaffu Chino <kaffu-chino@outlook.com>
2025-01-17 18:16:33 +0800

    增加返回顶部按钮

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@
     <el-header class="header" height="100%">
       <PageHeader />
     </el-header>
-    <el-main class="router-container"> <router-view></router-view></el-main>
+    <el-main class="router-container"> <router-view></router-view> </el-main>
+    <el-backtop :right="50" :bottom="50" />
     <el-footer class="footer" height="100%">
       <PageFooter />
     </el-footer>
